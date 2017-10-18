505dc6df1e8325c9b13002986d772d25d863d4ac
Ashish Shekar <ckrackerx15@gmail.com>
2017-10-18 16:07:16 +0530

    remove console on sorting param

--- routes/nnumber.js
+++ routes/nnumber.js
@@ -4,7 +4,6 @@ var general = require("../helpers/general");
 
 module.exports = function(req, res) {
 	general.txt().then(function(result) {
-		console.log(req.params.sort);
 		res.send(JSON.stringify(general.operate(result, req.params.number, req.params.sort)));
 	}).catch(function(reason) {
 		// res.send("errorr!");
